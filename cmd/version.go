74eca32200fcb6e0ce5ac60ff8e926a9bb6d381d
Tulio Ruiz <tulio@vauxoo.com>
2023-08-25 17:23:02 -0600

    [ADD] Files for the release

---
+++ cmd/version.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	Number    = "0.0.0-alpha.0"
+	Build     = "LocalBuild"
+	BuildDate = ""
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Shows version and build details",
+	Long:  `Show the current version, commit used to build the command and the build date.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Version: %s\n", Number)
+		fmt.Printf("Build: %s\n", Build)
+		fmt.Printf("Build date: %s\n", BuildDate)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
