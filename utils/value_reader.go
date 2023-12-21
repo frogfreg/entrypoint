8c00500df8643b86722a2c8a49d98920a09a98eb
Guillermo Fernández <guillermofr@vauxoo.com>
2023-12-21 17:02:09 -0600

    new function to get valueReader

--- utils/value_reader.go
+++ utils/value_reader.go
@@ -1,35 +1,65 @@
 package utils
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
 type valueReader interface {
-	readValue(string) (string, error)
+	readValue(string) string
 }
 
-type EnvGetter struct{}
-type FileGetter struct {
-	FileName string
-}
-type DockerSecretsGetter struct {
-	SecretsPath string
+type envGetter struct{}
+type valueStore struct {
+	source string
+	dict   map[string]string
 }
 
-func (eg *EnvGetter) readValue(key string) (string, error) {
-	return os.Getenv(key), nil
+func (eg *envGetter) readValue(key string) string {
+	return os.Getenv(key)
 }
 
-func (fg *FileGetter) readValue(key string) (string, error) {
-	keyValueMap, err := readFilePairs(fg.FileName)
+func (fg *valueStore) readValue(key string) string {
+	return fg.dict[key]
+}
+
+func (vs *valueStore) updateDict(f func(string) (map[string]string, error)) error {
+	keyValueMap, err := f(vs.source)
 	if err != nil {
-		return "", err
+		return err
 	}
-	return keyValueMap[key], nil
+	vs.dict = keyValueMap
+	return nil
 }
 
-func (dsg *DockerSecretsGetter) readValue(key string) (string, error) {
-	keyValueMap, err := readDockerSecrets(dsg.SecretsPath)
-	if err != nil {
-		return "", err
+func getValueReader() (valueReader, error) {
+	useDockerSecrets := false
+	if os.Getenv("ORCHESTSH_USE_DOCKER_SECRETS") != "" {
+		newValue, err := strconv.ParseBool(os.Getenv("ORCHESTSH_USE_DOCKER_SECRETS"))
+		if err != nil {
+			return nil, err
+		}
+		useDockerSecrets = newValue
 	}
-	return keyValueMap[key], nil
+
+	secretsPath := os.Getenv("ORCHESTSH_SECRETS_PATH")
+	useFile := os.Getenv("ORCHESTSH_USE_FILE")
+
+	switch {
+	case useDockerSecrets && secretsPath != "":
+		dockerSecretsVS := valueStore{source: secretsPath}
+		if err := dockerSecretsVS.updateDict(readDockerSecrets); err != nil {
+			return nil, err
+		}
+		return &dockerSecretsVS, nil
+
+	case useFile != "":
+		fileVS := valueStore{source: useFile}
+		if err := fileVS.updateDict(readFilePairs); err != nil {
+			return nil, err
+		}
+		return &fileVS, nil
+	default:
+		return &envGetter{}, nil
+	}
 }
--- utils/value_reader_test.go
+++ utils/value_reader_test.go
@@ -2,58 +2,66 @@ package utils
 
 import (
 	"os"
+	"reflect"
 	"testing"
 )
 
-func TestEnvGetter_readValue(t *testing.T) {
-	eg := EnvGetter{}
-
-	res, err := eg.readValue("test")
+func TestGetValueReader(t *testing.T) {
+	tempDir, err := os.MkdirTemp("", "testdir")
 	if err != nil {
 		t.Error(err)
 	}
-	if res != "" {
-		t.Errorf("expected %q, got %q", "", res)
-	}
-
-	os.Setenv("test", "true")
-
-	res, err = eg.readValue("test")
+	tempFile, err := os.CreateTemp(tempDir, "tempfile")
 	if err != nil {
 		t.Error(err)
-	}
-	if res != "true" {
-		t.Errorf("expected %q, got %q", "true", res)
-	}
 
-}
-
-func TestFileGetter_readValue(t *testing.T) {
-	tempFile, err := os.CreateTemp("", "test")
-	if err != nil {
+	}
+	if _, err := tempFile.WriteString("user=root\npassword=12345"); err != nil {
 		t.Error(err)
 	}
-	defer os.Remove(tempFile.Name())
+	defer os.RemoveAll(tempDir)
 
-	content := "test=true\n"
-	if _, err := tempFile.WriteString(content); err != nil {
-		t.Error(err)
+	tests := []struct {
+		name         string
+		expectedType valueReader
+		envVars      map[string]string
+	}{
+		{name: "envGetter",
+			expectedType: &envGetter{},
+			envVars:      map[string]string{"user": "root", "password": "12345"},
+		},
+		{name: "file valueStore",
+			expectedType: &valueStore{source: tempFile.Name()},
+			envVars:      map[string]string{"ORCHESTSH_USE_FILE": tempFile.Name()},
+		},
+		{name: "docker secrets valueStore",
+			expectedType: &valueStore{source: tempDir},
+			envVars:      map[string]string{"ORCHESTSH_USE_DOCKER_SECRETS": "true", "ORCHESTSH_SECRETS_PATH": tempDir},
+		},
 	}
 
-	fg := FileGetter{tempFile.Name()}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for k, v := range tt.envVars {
+				if err := os.Setenv(k, v); err != nil {
+					t.Error(err)
+				}
+			}
+			vr, err := getValueReader()
+			if err != nil {
+				t.Error(err)
+			}
+			if reflect.TypeOf(vr) != reflect.TypeOf(tt.expectedType) {
+				t.Errorf("expected %T, but got %T", tt.expectedType, vr)
+			}
+			if vr.readValue("user") != "root" {
+				t.Errorf("expected %q, but got %q", "root", vr.readValue("user"))
+			}
+			if vr.readValue("password") != "12345" {
+				t.Errorf("expected %q, but got %q", "12345", vr.readValue("password"))
+			}
 
-	res, err := fg.readValue("test")
-	if err != nil {
-		t.Error(err)
+		})
 	}
-	if res != "true" {
-		t.Errorf("expected %q, got %q", "true", res)
-	}
-	res, err = fg.readValue("not_present")
-	if err != nil {
-		t.Error(err)
-	}
-	if res != "" {
-		t.Errorf("expected %q, got %q", "", res)
-	}
+
 }
